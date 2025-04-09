dcbcc0bb47263f746f1097afe353953a409340b0
Justin cholo Adlawan <justincholo1234@gmail.com>
2025-04-09 15:45:23 +0800

    code written: created the functions needed for the program and layout of the code

---
+++ quiz_creator.py
@@ -0,0 +1,14 @@
+def questions_getter():
+    """ This function is responsible for getting questions from the user."""
+
+def choices():
+    """ This function is responsible for getting choices from the user."""
+
+def correct_answer():
+    """ This function is responsible for getting the correct answer from the user."""
+
+def quiz_question_saver():
+    """ This function is responsible for saving the quiz question to the text file."""
+
+def main_menu():
+    """ This function is responsible for displaying the main menu."""
