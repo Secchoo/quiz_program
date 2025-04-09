c87fe47522865187fb847d29f39d4e175baaa549
Justin cholo Adlawan <justincholo1234@gmail.com>
2025-04-09 16:31:07 +0800

    code edited: used for and while loop for getting the 4 choices

--- quiz_creator.py
+++ quiz_creator.py
@@ -2,7 +2,7 @@ def questions_getter():
     """ This function is responsible for getting questions from the user."""
 
     #prompts the user to enter a question
-    question = input("Enter the question: ")
+    question = input("\nEnter the question: ")
 
     #makes sure that the question is not empty
     if question == "":
@@ -18,18 +18,25 @@ def choices():
     """ This function is responsible for getting choices from the user."""
 
     #prompts the user to enter the choices
-    choice1 = input("Enter choice 1: ")
-    choice2 = input("Enter choice 2: ")
-    choice3 = input("Enter choice 3: ")
-    choice4 = input("Enter choice 4: ")
-
-    #makes sure that the choices are not empty
-    if choice1 == "" or choice2 == "" or choice3 == "" or choice4 == "":
-        print("Choices cannot be empty.")
-        return choices()
+    answers = []
+    for i in range(4):
+        while True:
+            answer = input(f"Enter choice {i+1}: ")
+            if answer:
+                answers.append(answer)
+                break
+            print("Choice cannot be empty.")
+        return answers
 
 def correct_answer():
     """ This function is responsible for getting the correct answer from the user."""
+    
+    #prompts the user to enter the correct answer
+    answer = input("Enter the correct answer: ")
+
+    #
+
+
 
 def quiz_question_saver():
     """ This function is responsible for saving the quiz question to the text file."""
